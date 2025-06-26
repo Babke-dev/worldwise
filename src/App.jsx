90a006bdc7559d7eb53f950fa35aae5a2c3989a2
Babke-dev <babic.al@outlook.com>
2025-06-26 11:14:42 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Navigate, BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
+import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
+import { CitiesProvider } from './contexts/CitiesContext';
+
+// const BASE_URL = 'http://localhost:8000/';
+
+function App() {
+  return (
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
+  );
+}
+
+export default App;
